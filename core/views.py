45419d10f043879988ebbc48e5ae08fd5e07bf1b
Guilherme Kauan <guilhermekauamt01@gmail.com>
2026-01-13 15:13:55 -0400

    13/01/2026

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "core/index.html")
+    return render(request, 'core/index.html')
+
+def cardapio(request):
+    return render(request, 'core/cardapio.html')
