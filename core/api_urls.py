8592f43253a160ab1ea74d91d0ddc65b9969079f
Guilherme Kauan <123951833+devgui01@users.noreply.github.com>
2026-02-05 14:33:43 -0400

    Delete core directory

--- core/api_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from . import api_views
-
-router = DefaultRouter()
-router.register(r'produtos', api_views.ProdutoViewSet, basename='produto')
-router.register(r'categorias', api_views.CategoriaViewSet, basename='categoria')
-
-urlpatterns = router.urls
--- core/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'core/index.html')
-
-def cardapio(request):
-    return render(request, 'core/cardapio.html')
